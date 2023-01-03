da2e6e3256af2931f9682d4c75ffc3741b7bd92c
Mitchell Stahl <mitchellstahl@Mitchells-MBP.attlocal.net>
2023-01-03 05:21:12 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
+});
+
+//scroll delay animation
+
+let loader = document.querySelector(".loader");
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+  loader.classList.add("disappear");
+}
+
+//number counter animation
+
+let valueDisplays = document.querySelectorAll(".num");
+
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
